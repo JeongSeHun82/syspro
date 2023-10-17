6f3d794dd45f09baa36aee24949f03d77a34b81a
SeHunJeong <sksk0001268@gmail.com>
2023-10-18 02:27:57 +0900

    assignment

--- README.md
+++
@@ -1,2 +0,0 @@
-# syspro
-Git training test 
--- chap4/prob4/main.c
+++ chap4/prob4/main.c
@@ -13,8 +13,8 @@ int main(int argc, char* argv[])
 
 	fp = fopen(argv[1], "w");
 	printf("%-9s %-7s %-4s\n", "StudentID", "Name", "Score");
-	while (scanf("%d %s %d", &rec.id, rec.name, &rec.score)==3)
-		fprintf(fp, "%d %s %d ", rec.id, rec.name, rec.score);
+	while (fscanf(fp,"%d %s %d", &rec.id, rec.name, &rec.score)==3)
+		printf("%d %s %d ", rec.id, rec.name, rec.score);
 
 	fclose(fp);
 	return 0;
