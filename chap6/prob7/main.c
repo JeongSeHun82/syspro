c366957ce797e706ebc156fec3232ba181ea1f66
Your Name <you@example.com>
2023-11-08 07:00:50 +0000

    assignemnt

--- chap6/prob7/main.c
+++ chap6/prob7/main.c
@@ -1,10 +1,10 @@
-#include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
 
 int main(int argc, char *argv[ ])
 {
-	if (link(argv[1], argv[2]) == -1) {
+	int unlink();
+	if (unlink(argv[1]) == -1) {
 		perror(argv[1]);
 		exit(1);
 	}
