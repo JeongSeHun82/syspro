c2f1d62ee4860799bcbaf551a0045e743a957135
SeHunJeong <sksk0001268@gmail.com>
2023-12-08 20:52:12 +0900

    hello

---
+++ chap11/prob9/almhandler.c
@@ -0,0 +1,33 @@
+#include <stdio.h>
+#include <signal.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+void alarmHandler(int signo);
+
+int main() {
+    int sec = 0;
+
+    struct sigaction sa;
+    sa.sa_handler = alarmHandler;
+    sigemptyset(&sa.sa_mask);
+    sa.sa_flags = 0;
+
+    if (sigaction(SIGALRM, &sa, NULL) == -1) {
+        perror("sigaction");
+        exit(EXIT_FAILURE);
+    }
+
+    alarm(5);
+    printf("Infinite loop\n");
+    while (1) {
+        sleep(1);
+        printf("%d second\n", ++sec);
+    }
+    printf("Run error\n");
+}
+
+void alarmHandler(int signo) {
+    printf("Wake up\n");
+    exit(0);
+}
